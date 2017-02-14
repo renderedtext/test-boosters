11de91e20cdac303bd7d2013b0b64146fd4da887
Predrag Rakic <prakic@renderedtext.com>
2017-02-14 15:07:19 +0100

    RspecBooster class

--- spec/test_boosters_spec.rb
+++ spec/test_boosters_spec.rb
@@ -6,70 +6,81 @@ describe TestBoosters do
   end
 
   context "test file sorting" do
+    before(:context) do
+      @booster = RspecBooster.new(0)
+    end
+
     it "tests for empty array" do
-      expect(sort_by_size([])).to eq([])
+      expect(@booster.sort_by_size([])).to eq([])
     end
 
     it "tests single element array" do
-      expect(sort_by_size([a])).to eq([a])
+      expect(@booster.sort_by_size([a])).to eq([a])
     end
 
     it "tests for non-existent file in array" do
-      expect(sort_by_size(["non-existent"])).to eq([])
+      expect(@booster.sort_by_size(["non-existent"])).to eq([])
     end
 
     it "tests regular input" do
       input    = input_specs
       expected = expected_specs
-      expect(sort_by_size(input)).to eq(expected)
+      expect(@booster.sort_by_size(input)).to eq(expected)
     end
 
     it "tests regular input with non-existent files" do
       input    = input_specs
       expected = expected_specs
-      expect(sort_by_size(input)).to eq(expected)
+      expect(@booster.sort_by_size(input)).to eq(expected)
     end
   end
 
   context "test select_leftover_specs()" do
+    before(:context) do
+      @booster = RspecBooster.new(0)
+    end
+
     it "no leftover specs" do
-      expect(select_leftover_specs([], 3, 0)).to eq([])
+      expect(@booster.select_leftover_specs([], 3)).to eq([])
     end
 
     it "1 leftover spec, 3 threads, index 0" do
-      expect(select_leftover_specs([a], 3, 0)).to eq([a])
+      expect(@booster.select_leftover_specs([a], 3)).to eq([a])
     end
 
     it "1 leftover spec, 3 threads, index 2" do
-      expect(select_leftover_specs([a], 3, 2)).to eq([])
+      booster = RspecBooster.new(2)
+      expect(booster.select_leftover_specs([a], 3)).to eq([])
     end
 
     it "3 leftover specs, 2 threads, index 0" do
       input    = input_specs
       expected = [a, b]
-      expect(select_leftover_specs(input, 2, 0)).to eq(expected)
+      expect(@booster.select_leftover_specs(input, 2)).to eq(expected)
     end
 
     it "3 leftover specs, 2 threads, index 1" do
       input    = input_specs
       expected = [c]
-      expect(select_leftover_specs(input, 2, 1)).to eq(expected)
+      booster = RspecBooster.new(1)
+      expect(booster.select_leftover_specs(input, 2)).to eq(expected)
     end
 
     it "3 leftover specs, 3 threads, index 0" do
       input    = input_specs
       expected = [a]
-      expect(select_leftover_specs(input, 3, 0)).to eq(expected)
+      expect(@booster.select_leftover_specs(input, 3)).to eq(expected)
     end
 
     it "3 leftover specs, 3 threads, index 2" do
       input    = input_specs
       expected = [b]
-      expect(select_leftover_specs(input, 3, 2)).to eq(expected)
+      booster = RspecBooster.new(2)
+      expect(booster.select_leftover_specs(input, 3)).to eq(expected)
     end
   end
 
-  context "test run()" do
+  context "test select()" do
     it "2 threads running in thread 1, no scheduled specs, 3 leftover specs" do
       expected = [a, b]
       report = '[{"files": []}, {"files": []}]'
@@ -78,7 +89,7 @@ describe TestBoosters do
       ENV["REPORT_PATH"] = report_file
       ENV["SPEC_PATH"]   = "test_data"
 
-      expect(run(0)).to eq(expected)
+      expect(RspecBooster.new(0).select).to eq(expected)
     end
 
     it "2 threads running in thread 2, no scheduled specs, 3 leftover specs" do
@@ -89,7 +100,7 @@ describe TestBoosters do
       ENV["REPORT_PATH"] = report_file
       ENV["SPEC_PATH"]   = "test_data"
 
-      expect(run(1)).to eq(expected)
+      expect(RspecBooster.new(1).select).to eq(expected)
     end
 
     it "4 threads running in thread 4, no scheduled specs, 3 leftover specs" do
@@ -100,7 +111,18 @@ describe TestBoosters do
       ENV["REPORT_PATH"] = report_file
       ENV["SPEC_PATH"]   = "test_data"
 
-      expect(run(3)).to eq(expected)
+      expect(RspecBooster.new(3).select).to eq(expected)
+    end
+
+    it "4 threads, running in thread 5, no scheduled specs, 3 leftover specs" do
+      expected = []
+      report = '[{"files": []}, {"files": []}, {"files": []}, {"files": []}]'
+      report_file = "/tmp/rspec_report.json"
+      File.write(report_file, report)
+      ENV["REPORT_PATH"] = report_file
+      ENV["SPEC_PATH"]   = "test_data"
+
+      expect(RspecBooster.new(4).select).to eq("error")
     end
   end
 
