a17eb454165cfb0ff3c86b19a716a174553c9e51
Igor Šarčević <igor@renderedtext.com>
2017-03-17 18:46:24 +0100

    Use split configuration in rspec

--- lib/test_boosters/rspec_booster.rb
+++ lib/test_boosters/rspec_booster.rb
@@ -4,7 +4,6 @@ module TestBoosters
 
     def initialize(thread_index)
       @thread_index = thread_index
-      @rspec_split_configuration_path = ENV["RSPEC_SPLIT_CONFIGURATION_PATH"] || "#{ENV["HOME"]}/rspec_split_configuration.json"
       @report_path = ENV["REPORT_PATH"] || "#{ENV["HOME"]}/rspec_report.json"
       @spec_path = ENV["SPEC_PATH"] || "spec"
     end
@@ -39,18 +38,18 @@ module TestBoosters
 
     def select
       with_fallback do
-        file_distribution = JSON.parse(File.read(@rspec_split_configuration_path))
-        thread_count = file_distribution.count
-        thread = file_distribution[@thread_index]
+        split_configuration = TestBoosters::SplitConfiguration.for_rspec
+        thread_count = split_configuration.threads.count
 
         puts "Running RSpec thread #{@thread_index + 1} out of #{thread_count} threads\n"
 
         all_specs = Dir["#{@spec_path}/**/*_spec.rb"].sort
-        all_known_specs = file_distribution.map { |t| t["files"] }.flatten.sort
+        all_known_specs = split_configuration.all_files
 
         all_leftover_specs = all_specs - all_known_specs
         thread_leftover_specs = TestBoosters::LeftoverFiles.select(all_leftover_specs, thread_count, @thread_index)
-        thread_specs = all_specs & thread["files"].sort
+
+        thread_specs = all_specs & split_configuration.files_for_thread(@thread_index)
         specs_to_run = thread_specs + thread_leftover_specs
 
         TestBoosters::Shell.display_files("This thread specs:", thread_specs)
