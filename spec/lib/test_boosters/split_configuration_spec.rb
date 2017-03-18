8dd8522fac37618a8b70014006a8dcfbf01efb97
Igor Šarčević <igor@renderedtext.com>
2017-03-18 12:23:01 +0100

    Fix split configuration

--- spec/lib/test_boosters/split_configuration_spec.rb
+++ spec/lib/test_boosters/split_configuration_spec.rb
@@ -5,7 +5,7 @@ describe TestBoosters::SplitConfiguration do
   describe ".for_rspec" do
     context "env var points to file location" do
       before do
-        content = [ { :files => ["dragon_ball_z_spec.rb"] } ]
+        content = [{ :files => ["dragon_ball_z_spec.rb"] }]
 
         @path = "/tmp/split_configuration"
 
@@ -14,10 +14,10 @@ describe TestBoosters::SplitConfiguration do
         File.write(@path, content.to_json)
       end
 
-      subject { TestBoosters::SplitConfiguration.new(@path) }
+      subject(:configuration) { TestBoosters::SplitConfiguration.new(@path) }
 
       it "loads from the file pointed by the env var" do
-        expect(subject.all_files).to include("dragon_ball_z_spec.rb")
+        expect(configuration.all_files).to include("dragon_ball_z_spec.rb")
       end
     end
   end
@@ -25,7 +25,7 @@ describe TestBoosters::SplitConfiguration do
   describe ".for_cucumber" do
     context "env var points to file location" do
       before do
-        content = [ { :files => ["dragon_ball_z.feature"] } ]
+        content = [{ :files => ["dragon_ball_z.feature"] }]
 
         @path = "/tmp/split_configuration"
 
@@ -34,28 +34,28 @@ describe TestBoosters::SplitConfiguration do
         File.write(@path, content.to_json)
       end
 
-      subject { TestBoosters::SplitConfiguration.new(@path) }
+      subject(:configuration) { TestBoosters::SplitConfiguration.new(@path) }
 
       it "loads from the file pointed by the env var" do
-        expect(subject.all_files).to include("dragon_ball_z.feature")
+        expect(configuration.all_files).to include("dragon_ball_z.feature")
       end
     end
   end
 
   context "file does not exists" do
-    subject { TestBoosters::SplitConfiguration.new("/tmp/non_existing_file_path") }
+    subject(:configuration) { TestBoosters::SplitConfiguration.new("/tmp/non_existing_file_path") }
 
-    it { is_expected.to_not be_present }
+    it { is_expected.not_to be_present }
 
     describe "#all_files" do
-      it "should be an empty array" do
-        expect(subject.all_files).to eq([])
+      it "is an empty array" do
+        expect(configuration.all_files).to eq([])
       end
     end
 
     describe "#threads" do
-      it "should be an empty array" do
-        expect(subject.threads).to eq([])
+      it "is an empty array" do
+        expect(configuration.threads).to eq([])
       end
     end
   end
@@ -65,7 +65,7 @@ describe TestBoosters::SplitConfiguration do
       content = [
         { :files => ["a_spec.rb", "d_spec.rb", "c_spec.rb"] },
         { :files => ["f_spec.rb", "b_spec.rb"] },
-        { :files => [] },
+        { :files => [] }
       ]
 
       @path = "/tmp/split_configuration"
@@ -73,33 +73,33 @@ describe TestBoosters::SplitConfiguration do
       File.write(@path, content.to_json)
     end
 
-    subject { TestBoosters::SplitConfiguration.new(@path) }
+    subject(:configuration) { TestBoosters::SplitConfiguration.new(@path) }
 
     it { is_expected.to be_present }
 
     describe "#all_files" do
-      it "should return all files from the split configuration" do
-        expect(subject.all_files).to eq([
+      it "returns all files from the split configuration" do
+        expect(configuration.all_files).to eq([
           "a_spec.rb",
           "b_spec.rb",
           "c_spec.rb",
           "d_spec.rb",
-          "f_spec.rb",
+          "f_spec.rb"
         ])
       end
     end
 
     describe "#threads" do
       it "returns instances of TestBoosters::SplitConfiguration::Thread" do
-        subject.threads.each do |thread|
+        configuration.threads.each do |thread|
           expect(thread).to be_instance_of(TestBoosters::SplitConfiguration::Thread)
         end
       end
 
       it "puts every file to its proper thread instance" do
-        expect(subject.threads[0].files).to eq ["a_spec.rb", "c_spec.rb", "d_spec.rb"]
-        expect(subject.threads[1].files).to eq ["b_spec.rb", "f_spec.rb"]
-        expect(subject.threads[2].files).to eq []
+        expect(configuration.threads[0].files).to eq ["a_spec.rb", "c_spec.rb", "d_spec.rb"]
+        expect(configuration.threads[1].files).to eq ["b_spec.rb", "f_spec.rb"]
+        expect(configuration.threads[2].files).to eq []
       end
     end
   end
