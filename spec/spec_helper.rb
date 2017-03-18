122948f6c0165dc0b6cfd2f19ff996b0822e9b7a
Igor Šarčević <igor@renderedtext.com>
2017-03-18 12:27:32 +0100

    Fix spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,30 +1,59 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'test_boosters'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "test_boosters"
 
 module Setup
   module_function
 
-  def spec_dir() "test_data" end
+  def spec_dir
+    "test_data"
+  end
 
-  def a() "test_data/a_spec.rb" end
-  def b() "test_data/b_spec.rb" end
-  def c() "test_data/c_spec.rb" end
+  def a
+    "test_data/a_spec.rb"
+  end
 
-  def input_specs()     [a, b, c]  end
+  def b
+    "test_data/b_spec.rb"
+  end
 
-  def expected_specs()  [a, c, b]  end
+  def c
+    "test_data/c_spec.rb"
+  end
+
+  def input_specs
+    [a, b, c]
+  end
+
+  def expected_specs
+    [a, c, b]
+  end
 
   module Cucumber
     module_function
 
-    def feature_dir() "test_data" end
+    def feature_dir
+      "test_data"
+    end
 
-    def a() "test_data/a.feature" end
-    def b() "test_data/b.feature" end
-    def c() "test_data/c.feature" end
+    def a
+      "test_data/a.feature"
+    end
 
-    def input_specs()     [a, b, c]  end
+    def b
+      "test_data/b.feature"
+    end
 
-    def expected_specs()  [a, c, b]  end
+    def c
+      "test_data/c.feature"
+    end
+
+    def input_specs
+      [a, b, c]
+    end
+
+    def expected_specs
+      [a, c, b]
+    end
+
   end
 end
