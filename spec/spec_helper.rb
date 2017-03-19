3aa4f42ab204288a286912ae3229ca1321356ed6
Igor Šarčević <igor@renderedtext.com>
2017-03-19 01:50:26 +0100

    Ignore feature files in rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,3 +33,60 @@ RSpec.configure do |config|
   end
 
 end
+
+module Setup
+  module_function
+
+  def spec_dir
+    "test_data"
+  end
+
+  def a
+    "test_data/a_spec.rb"
+  end
+
+  def b
+    "test_data/b_spec.rb"
+  end
+
+  def c
+    "test_data/c_spec.rb"
+  end
+
+  def input_specs
+    [a, b, c]
+  end
+
+  def expected_specs
+    [a, c, b]
+  end
+
+  module Cucumber
+    module_function
+
+    def feature_dir
+      "test_data"
+    end
+
+    def a
+      "test_data/a.feature"
+    end
+
+    def b
+      "test_data/b.feature"
+    end
+
+    def c
+      "test_data/c.feature"
+    end
+
+    def input_specs
+      [a, b, c]
+    end
+
+    def expected_specs
+      [a, c, b]
+    end
+
+  end
+end
