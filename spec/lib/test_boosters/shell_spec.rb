65a5eafa1244421889103f7898b5ab0de3fc9716
Igor Šarčević <igor@renderedtext.com>
2017-03-24 18:06:12 +0100

    Display executed command

---
+++ spec/lib/test_boosters/shell_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe TestBoosters::Shell do
+
+  describe ".execute" do
+    it "returns the exit status of the command" do
+      result = described_class.execute("false")
+
+      expect(result).to eq(1)
+    end
+
+    it "displays the executed command" do
+      expect { described_class.execute("echo 'here'") }.to output(/echo 'here'/).to_stdout
+    end
+
+    it "displays the output of the command" do
+      expect { described_class.execute("echo 'yo'") }.to output(/yo/).to_stdout_from_any_process
+    end
+
+    describe "silent execution" do
+      it "doesn't display the command on stdout" do
+        expect { described_class.execute("echo 'here'", :silent => true) }.to_not output(/echo 'here'/).to_stdout
+      end
+
+      it "displays the output of the command" do
+        expect { described_class.execute("echo 'yo'", :silent => true) }.to output(/yo/).to_stdout_from_any_process
+      end
+    end
+  end
+
+end
