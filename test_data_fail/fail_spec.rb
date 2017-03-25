8c0a7059ed660075040c464b0769b9f4d94c2aaf
Igor Šarčević <igor@renderedtext.com>
2017-03-25 11:58:05 +0100

    Remove deprecated test directories

--- test_data_fail/fail_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe TestBoosters::RspecBooster do
-  it 'fails' do
-    expect(TestBoosters::VERSION).to be "wrong"
-  end
-end
--- test_data_pass/pass_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe TestBoosters::RspecBooster do
-  it 'passes' do
-    expect(0).to eq(0)
-  end
-end
