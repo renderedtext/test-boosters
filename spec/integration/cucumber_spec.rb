6d88d691698dd19019f3332ec5d0ae8abf123bb9
Igor Šarčević <igor@renderedtext.com>
2017-03-29 18:17:22 +0200

    Use external test repo

--- spec/integration/cucumber_spec.rb
+++ spec/integration/cucumber_spec.rb
@@ -2,81 +2,48 @@ require "spec_helper"
 
 describe "Cucumber Booster" do
 
-  let(:specs_path) { "features" }
-  let(:split_configuration_path) { "/tmp/cucumber_split_configuration.json" }
-  let(:cucumber_report_path) { "/tmp/cucumber_report.json" }
+  before(:all) do
+    @repo_path = "/tmp/test-boosters-tests"
+    @project_path = "/tmp/test-boosters-tests/cucumber_project"
+    @split_configuration_path = "/tmp/cucumber_split_configuration.json"
 
-  before do
-    # Set up environment variables
-    ENV["SPEC_PATH"] = specs_path
-    ENV["CUCUMBER_SPLIT_CONFIGURATION_PATH"] = split_configuration_path
-    ENV["REPORT_PATH"] = cucumber_report_path
+    ENV["CUCUMBER_SPLIT_CONFIGURATION_PATH"] = @split_configuration_path
 
-    # set up features directory
-    FileUtils.rm_f(cucumber_report_path)
-    FileUtils.rm_rf("features")
-    FileUtils.mkdir_p("features")
-    FileUtils.rm_rf("config")
-    FileUtils.mkdir_p("config")
-    File.write("config/cucumber.yml", "default: --format pretty\n")
-
-    # Create spec files
-    Support::CucumberFilesFactory.create(:name => "A", :path => "#{specs_path}/a.feature", :result => :failing)
-    Support::CucumberFilesFactory.create(:name => "B", :path => "#{specs_path}/b.feature", :result => :failing)
-    Support::CucumberFilesFactory.create(:name => "C", :path => "#{specs_path}/admin/c.feature", :result => :passing)
-
-    # Construct a split configuration
-    File.write(split_configuration_path, [
-      { :files => ["#{specs_path}/a.feature"] },
-      { :files => ["#{specs_path}/b.feature"] },
-      { :files => [] }
+    File.write(@split_configuration_path, [
+      { :files => [] },
+      { :files => ["features/a.feature"] }
     ].to_json)
 
-    # make sure that everything is set up as it should be
-    expect(File.exist?(cucumber_report_path)).to eq(false)
-
-    expect(File.exist?(split_configuration_path)).to eq(true)
-
-    expect(Dir["#{specs_path}/**/*"].sort).to eq([
-      "#{specs_path}/a.feature",
-      "#{specs_path}/admin",
-      "#{specs_path}/admin/c.feature",
-      "#{specs_path}/b.feature",
-      "#{specs_path}/step_definitions",
-      "#{specs_path}/step_definitions/a_step.rb",
-      "#{specs_path}/step_definitions/b_step.rb",
-      "#{specs_path}/step_definitions/c_step.rb"
-    ])
+    system("[ ! -e #{@repo_path} ] && git clone https://github.com/renderedtext/test-boosters-tests.git #{@repo_path}")
   end
 
-  specify "first job's behaviour" do
-    output = `cucumber_booster --job 1/3`
+  before { FileUtils.rm_f("#{ENV["HOME"]}/cucumber_report.json") }
 
-    expect(output).to include("Feature: A")
-    expect(output).to include("Feature: C")
-    expect(output).to include("2 scenarios (1 failed, 1 passed)")
+  specify "first job's behaviour" do
+    output = `cd #{@project_path} && cucumber_booster --job 1/3`
+
+    expect(output).to include("2 examples, 1 failure")
     expect($?.exitstatus).to eq(1)
 
-    expect(File.exist?(cucumber_report_path)).to eq(true)
+    expect(File).to exist("#{ENV["HOME"]}/cucumber_report.json")
   end
 
   specify "second job's behaviour" do
-    output = `cucumber_booster --job 2/3`
+    output = `cd #{@project_path} && cucumber_booster --job 2/3`
 
-    expect(output).to include("Feature: B")
-    expect(output).to include("1 scenario (1 failed)")
-    expect($?.exitstatus).to eq(1)
+    expect(output).to include("1 example, 0 failures")
+    expect($?.exitstatus).to eq(0)
 
-    expect(File.exist?(cucumber_report_path)).to eq(true)
+    expect(File).to exist("#{ENV["HOME"]}/cucumber_report.json")
   end
 
   specify "third job's behaviour" do
-    output = `cucumber_booster --job 3/3`
+    output = `cd #{@project_path} && cucumber_booster --job 3/3`
 
     expect(output).to include("No files to run in this job!")
     expect($?.exitstatus).to eq(0)
 
-    expect(File.exist?(cucumber_report_path)).to eq(false)
+    expect(File).to_not exist("#{ENV["HOME"]}/cucumber_report.json")
   end
 
 end
--- spec/integration/rspec_spec.rb
+++ spec/integration/rspec_spec.rb
@@ -2,76 +2,48 @@ require "spec_helper"
 
 describe "RSpec Booster" do
 
-  let(:project_path) { "/tmp/project_path-#{SecureRandom.uuid}" }
-  let(:specs_path) { "#{project_path}/spec" }
-  let(:split_configuration_path) { "/tmp/rspec_split_configuration.json" }
-  let(:rspec_report_path) { "/tmp/rspec_report.json" }
+  before(:all) do
+    @repo_path = "/tmp/test-boosters-tests"
+    @project_path = "/tmp/test-boosters-tests/rspec_project"
+    @split_configuration_path = "/tmp/rspec_split_configuration.json"
 
-  before do
-    # Set up environment variables
-    ENV["SPEC_PATH"] = specs_path
-    ENV["RSPEC_SPLIT_CONFIGURATION_PATH"] = split_configuration_path
-    ENV["REPORT_PATH"] = rspec_report_path
+    ENV["RSPEC_SPLIT_CONFIGURATION_PATH"] = @split_configuration_path
 
-    # Set up test dir structure
-    FileUtils.rm_rf(specs_path)
-    FileUtils.mkdir_p(specs_path)
-    FileUtils.rm_f(rspec_report_path)
-
-    # Create spec files
-    Support::RspecFilesFactory.create(:path => "#{specs_path}/a_spec.rb", :result => :failing)
-    Support::RspecFilesFactory.create(:path => "#{specs_path}/b_spec.rb", :result => :failing)
-    Support::RspecFilesFactory.create(:path => "#{specs_path}/lib/c_spec.rb", :result => :passing)
-
-    # Construct spec helper file
-    File.write("#{specs_path}/spec_helper.rb", "")
-
-    # Construct a split configuration
-    File.write(split_configuration_path, [
-      { :files => ["spec/a_spec.rb"] },
-      { :files => ["spec/b_spec.rb"] },
-      { :files => [] }
+    File.write(@split_configuration_path, [
+      { :files => [] },
+      { :files => ["spec/lib/a_spec.rb"] }
     ].to_json)
 
-    # make sure that everything is set up as it should be
-    expect(File.exist?(rspec_report_path)).to eq(false)
-
-    expect(File.exist?(split_configuration_path)).to eq(true)
-
-    expect(Dir["#{specs_path}/**/*"].sort).to eq([
-      "#{specs_path}/a_spec.rb",
-      "#{specs_path}/b_spec.rb",
-      "#{specs_path}/lib",
-      "#{specs_path}/lib/c_spec.rb",
-      "#{specs_path}/spec_helper.rb"
-    ])
+    system("[ ! -e #{@repo_path} ] && git clone https://github.com/renderedtext/test-boosters-tests.git #{@repo_path}")
   end
 
+  before { FileUtils.rm_f("#{ENV["HOME"]}/rspec_report.json") }
+
   specify "first job's behaviour" do
-    output = `cd #{project_path} && rspec_booster --job 1/3`
+    output = `cd #{@project_path} && rspec_booster --job 1/3`
 
     expect(output).to include("2 examples, 1 failure")
     expect($?.exitstatus).to eq(1)
 
-    expect(File.exist?(rspec_report_path)).to eq(true)
+    expect(File).to exist("#{ENV["HOME"]}/rspec_report.json")
   end
 
   specify "second job's behaviour" do
-    output = `cd #{project_path} && rspec_booster --job 2/3`
+    output = `cd #{@project_path} && rspec_booster --job 2/3`
 
-    expect(output).to include("1 example, 1 failure")
-    expect($?.exitstatus).to eq(1)
+    expect(output).to include("1 example, 0 failures")
+    expect($?.exitstatus).to eq(0)
 
-    expect(File.exist?(rspec_report_path)).to eq(true)
+    expect(File).to exist("#{ENV["HOME"]}/rspec_report.json")
   end
 
   specify "third job's behaviour" do
-    output = `cd #{project_path} && rspec_booster --job 3/3`
+    output = `cd #{@project_path} && rspec_booster --job 3/3`
 
     expect(output).to include("No files to run in this job!")
     expect($?.exitstatus).to eq(0)
 
-    expect(File.exist?(rspec_report_path)).to eq(false)
+    expect(File).to_not exist("#{ENV["HOME"]}/rspec_report.json")
   end
 
 end
