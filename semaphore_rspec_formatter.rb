ee50691144b30ba318e5d6bea758f07630f53db9
Igor Šarčević <igor@renderedtext.com>
2017-07-20 12:57:45 +0200

    Custom formatter that calculates shared examples correctly

---
+++ semaphore_rspec_formatter.rb
@@ -0,0 +1,58 @@
+RSpec::Support.require_rspec_core "formatters/base_formatter"
+require 'json'
+
+class SemaphoreFormatter < RSpec::Core::Formatters::BaseFormatter
+  RSpec::Core::Formatters.register self, :stop, :close
+
+  attr_reader :output_hash
+
+  def initialize(output)
+    super
+    @output_hash = {}
+  end
+
+  def stop(notification)
+    @output_hash[:examples] = notification.examples.map do |example|
+      format_example(example).tap do |hash|
+        if e=example.exception
+          hash[:exception] = {
+            :class => e.class.name,
+            :message => e.message,
+            :backtrace => e.backtrace,
+          }
+        end
+      end
+    end
+  end
+
+  def close(notification)
+    output.write @output_hash.to_json
+    output.close if IO === output && output != $stdout
+  end
+
+  private
+
+  def format_example(example)
+    {
+      :description => example.description,
+      :full_description => example.full_description,
+      :status => example.execution_result.status,
+      :file_path => file_path(example),
+      :run_time => example.execution_result.run_time
+    }
+  end
+
+  def file_path(example)
+    # In case of a shared_example
+    # example[:file_path] returns the path of the shared_example file
+    #
+    # We are interested in the duration of the file from which the shared_example was called instead.
+    #
+    # We can get this infor from `example.id`.
+    #
+    # It has the following format: `./spec/models/analysis_spec.rb[1:17:1:1:1]`
+    # We are droping the angle brackets at the end.
+
+    example.id.gsub(/\[.*\]$/, "")
+  end
+end
