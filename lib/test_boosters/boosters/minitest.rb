3dc1c3d03ee2c1cdc68f9ec12b361e7fa8d6efb5
Igor Šarčević <igor@renderedtext.com>
2020-08-05 13:12:22 +0200

    Run Rails enclosed Minitest commands

--- lib/test_boosters/boosters/minitest.rb
+++ lib/test_boosters/boosters/minitest.rb
@@ -9,11 +9,31 @@ module TestBoosters
       end
 
       def command
-        "ruby -e 'ARGV.each { |f| require \"./\#{f}\" }'"
+        if command_set_with_env_var?
+          command_from_env_var
+        elsif rails_app?
+          "bundle exec rails test"
+        else
+          "ruby -e 'ARGV.each { |f| require \"./\#{f}\" }'"
+        end
       end
 
       def split_configuration_path
         ENV["MINITEST_SPLIT_CONFIGURATION_PATH"] || "#{ENV["HOME"]}/minitest_split_configuration.json"
+      end
+
+      def command_set_with_env_var?
+        !command_from_env_var.empty?
+      end
+
+      def command_from_env_var
+        ENV["MINITEST_BOOSTER_COMMAND"]
+      end
+
+      private
+
+      def rails_app?
+        File.exist?("app") && File.exist?("config") && File.exist?("config/application.rb")
       end
 
     end
